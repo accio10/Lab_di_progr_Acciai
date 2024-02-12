9adec6d6dbf94e911b1e5f8fa6b7630baeb22c44
Nathan <nathan.acciai@edu.unifi.it>
2024-02-12 11:11:06 +0100

    Aggiungiunto classe Transaction

---
+++ Transaction.h
@@ -0,0 +1,16 @@
+//
+// Created by Nathan on 12/02/2024.
+//
+
+#ifndef LAB_DI_PROGR_ACCIAI_TRANSACTION_H
+#define LAB_DI_PROGR_ACCIAI_TRANSACTION_H
+class Transaction{
+public:
+    Transaction( int tyf, int v);
+
+private:
+    int typeoftransaction;
+    int valueoftransaction;
+
+};
+#endif //LAB_DI_PROGR_ACCIAI_TRANSACTION_H
